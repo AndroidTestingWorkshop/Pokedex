0e01a4b81c21b1accca066cc760190ae373adcbb
Baskara Patria <baskara@go-jek.com>
2016-10-01 14:45:41 +0530

    Network Error knows how to handle no internet connection issues

--- app/src/main/java/gojek/pokedex/NetworkError.java
+++ app/src/main/java/gojek/pokedex/NetworkError.java
@@ -1,11 +1,17 @@
 package gojek.pokedex;
 
-public class NetworkError {
-    public NetworkError(Throwable exception) {
+import java.net.SocketTimeoutException;
 
+public class NetworkError {
+    private Throwable exception;
+
+    public NetworkError(Throwable exception) {
+        this.exception = exception;
     }
 
     public String getMessage() {
-        return "Server Timed Out, Please Try Again!";
+        if (exception instanceof SocketTimeoutException)
+            return "Server Timed Out, Please Try Again!";
+        return "No Internet Connection! Please Try Again!";
     }
 }
--- app/src/test/java/gojek/pokedex/NetworkErrorTest.java
+++ app/src/test/java/gojek/pokedex/NetworkErrorTest.java
@@ -5,6 +5,7 @@ import junit.framework.Assert;
 import org.junit.Test;
 
 import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
 
 public class NetworkErrorTest {
     @Test
@@ -12,5 +13,12 @@ public class NetworkErrorTest {
         SocketTimeoutException timeOutException = new SocketTimeoutException();
         NetworkError networkError = new NetworkError(timeOutException);
         Assert.assertEquals("Server Timed Out, Please Try Again!", networkError.getMessage());
+    }
+
+    @Test
+    public void testThatItHandlesInternetConnectionIssues() {
+        UnknownHostException noInternetConnectionPresent = new UnknownHostException();
+        NetworkError networkError = new NetworkError(noInternetConnectionPresent);
+        Assert.assertEquals("No Internet Connection! Please Try Again!", networkError.getMessage());
     }
 }
