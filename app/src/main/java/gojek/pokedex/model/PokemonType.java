904ada4ab62a5153147a50dc90ae038c34679069
Chirag <me@chi6rag.net>
2016-10-04 05:34:53 +0530

    Adding Pokemon Types

--- app/src/main/java/gojek/pokedex/model/PokemonType.java
+++ app/src/main/java/gojek/pokedex/model/PokemonType.java
@@ -3,19 +3,65 @@ package gojek.pokedex.model;
 import android.graphics.Color;
 import android.support.annotation.ColorInt;
 
+import gojek.pokedex.R;
+
 public enum PokemonType {
-    ELECTRICITY {
-        public @ColorInt int color() {
-            return Color.YELLOW;
-        }
-    },
     GRASS {
         public @ColorInt int color() {
-            return Color.GREEN;
+            return resolve(R.color.type_grass);
+        }
+    },
+    FIRE {
+        public @ColorInt int color() {
+            return resolve(R.color.type_fire);
+        }
+    },
+    WATER {
+        public @ColorInt int color() {
+            return resolve(R.color.type_water);
+        }
+    },
+    BUG {
+        public @ColorInt int color() {
+            return resolve(R.color.type_bug);
+        }
+    },
+    NORMAL {
+        public @ColorInt int color() {
+            return resolve(R.color.type_normal);
+        }
+    },
+    POISON {
+        public @ColorInt int color() {
+            return resolve(R.color.type_poison);
+        }
+    },
+    ELECTRIC {
+        public @ColorInt int color() {
+            return resolve(R.color.type_electric);
+        }
+    },
+    GROUND {
+        public @ColorInt int color() {
+            return resolve(R.color.type_ground);
+        }
+    },
+    FAIRY {
+        public @ColorInt int color() {
+            return resolve(R.color.type_fairy);
+        }
+    },
+    FIGHTING {
+        public @ColorInt int color() {
+            return resolve(R.color.type_fighting);
         }
     };
 
     public @ColorInt int color() {
         return this.color();
+    }
+
+    private static int resolve(int colorResource) {
+        return Color.parseColor(String.valueOf(colorResource));
     }
 }
